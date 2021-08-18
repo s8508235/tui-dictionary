d886e33c6b000f288a3c10ea827486aedae2b015
s8508235 <s8508235@yahoo.com.tw>
2021-08-19 00:15:49 +0800

    feat(dictionary): remove cache for searching web dictionaries

--- pkg/dictionary/factory.go
+++ pkg/dictionary/factory.go
@@ -2,6 +2,7 @@ package dictionary
 
 import (
 	"fmt"
+	"net/url"
 	"regexp"
 
 	"github.com/gocolly/colly/v2"
@@ -10,6 +11,36 @@ import (
 )
 
 var re = regexp.MustCompile(`(?s)[\s]+`)
+
+type emptyStorage struct{}
+
+// Init initializes emptyStorage
+func (s *emptyStorage) Init() error {
+	return nil
+}
+
+// Visited implements Storage.Visited()
+func (s *emptyStorage) Visited(requestID uint64) error {
+	return nil
+}
+
+// IsVisited implements Storage.IsVisited()
+func (s *emptyStorage) IsVisited(requestID uint64) (bool, error) {
+	return false, nil
+}
+
+// Cookies implements Storage.Cookies()
+func (s *emptyStorage) Cookies(u *url.URL) string {
+	return ""
+}
+
+// SetCookies implements Storage.SetCookies()
+func (s *emptyStorage) SetCookies(u *url.URL, cookies string) {}
+
+// Close implements Storage.Close()
+func (s *emptyStorage) Close() error {
+	return nil
+}
 
 func generalWebDictionarySearch(results *[]string, counter *int) func(e *colly.HTMLElement) {
 	return func(e *colly.HTMLElement) {
@@ -38,8 +69,11 @@ func NewDICTClient(logger *log.Logger, network, addr, prefix string) (*DICTClien
 }
 
 func NewCollinsDictionary(logger *log.Logger) Interface {
+	c := colly.NewCollector()
+	// don't want to cache anything since it should be a light query
+	c.SetStorage(&emptyStorage{})
 	return &WebDictionaryCrawler{
-		Crawler: colly.NewCollector(),
+		Crawler: c,
 		Logger:  logger,
 		SearchURL: func(word string) string {
 			return fmt.Sprintf(collinsURL, re.ReplaceAllString(word, "-"))
@@ -50,8 +84,11 @@ func NewCollinsDictionary(logger *log.Logger) Interface {
 }
 
 func NewUrbanDictionary(logger *log.Logger) Interface {
+	c := colly.NewCollector()
+	// don't want to cache anything since it should be a light query
+	c.SetStorage(&emptyStorage{})
 	return &WebDictionaryCrawler{
-		Crawler: colly.NewCollector(),
+		Crawler: c,
 		Logger:  logger,
 		SearchURL: func(word string) string {
 			return fmt.Sprintf(urbanURL, re.ReplaceAllString(word, "%20"))
@@ -62,8 +99,11 @@ func NewUrbanDictionary(logger *log.Logger) Interface {
 }
 
 func NewLearnerDictionary(logger *log.Logger) Interface {
+	c := colly.NewCollector()
+	// don't want to cache anything since it should be a light query
+	c.SetStorage(&emptyStorage{})
 	return &WebDictionaryCrawler{
-		Crawler: colly.NewCollector(),
+		Crawler: c,
 		Logger:  logger,
 		SearchURL: func(word string) string {
 			return fmt.Sprintf(learnerURL, re.ReplaceAllString(word, "%20"))
